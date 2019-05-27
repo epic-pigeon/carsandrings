f7e6338b795e7bc008f5b8d45c972e508b729125
Tony <boreykotony@gmail.com>
2019-05-27 14:35:32 +0300

    kar

--- Cars And Rings/Assets/Scripts/CarController.cs
+++ Cars And Rings/Assets/Scripts/CarController.cs
@@ -14,6 +14,9 @@ public class CarController : MonoBehaviour
     public float acceleration;
     private float currentAngleSpeed;
     private int temp;
+    private bool isDrift;
+    private Vector3 vector;
+    private Vector3 vecLast;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,7 @@ public class CarController : MonoBehaviour
         acceleration = 0f;
         currentAngleSpeed = 0f;
         temp = 0;
+        isDrift = false;
     }
 
     // Update is called once per frame
@@ -38,7 +42,7 @@ public class CarController : MonoBehaviour
             acceleration /= 1.02f;
         }
 
-        //Добавляем к ускорению нажатие клавишь
+        //Добавляем к ускорению нажатие клавиш
         if (vertical * acceleration < 0)
         {
             acceleration += 5 * vertical;
@@ -64,49 +68,32 @@ public class CarController : MonoBehaviour
         {
             acceleration = (Math.Abs(acceleration) / acceleration) * 500f;
         }
-        //TODO ДРИФТ
-        /*if (currentSpeed > 40f && horizontal != 0)
+
+
+        transform.rotation *= Quaternion.Euler(0f, (currentSpeed / speed) * horizontal * angleSpeed * Time.deltaTime, 0f);
+
+        if ((currentSpeed > 30f && horizontal != 0) || isDrift)
         {
-            transform.rotation *= Quaternion.Euler(0f, 360 * Time.deltaTime, 0f);
-            transform.position += new Vector3(1 , 0f, 1);
-            Debug.Log("Drift");
+            Debug.Log("kar");
+            float angle = 0;
+                float k = 0;
+            if (!isDrift) {
+                 angle = transform.rotation.y;
+                 k = vertical > 0 ? 1 : -1;
+            }
+            isDrift = true;
+            float module = 5f * vertical;
+            Vector3 acceleration2 = new Vector3(k * (float)Math.Sqrt(module * module / (1 + Math.Sin(angle) * Math.Sin(angle))), 0, k * (float)(Math.Sqrt(module * module / (1 + Math.Sin(angle) * Math.Sin(angle))) * Math.Sin(angle)));
+            transform.position += acceleration2;
+            acceleration /= 1.02f;
+            if (currentSpeed < 10f) {
+                isDrift = false;
+            }
         }
         else
-        {*/
-            /*if (Math.Abs(horizontal * currentSpeed) > 20f || Math.Abs(currentAngleSpeed) > 5f)
-            {
-                if (Math.Abs(horizontal * currentAngleSpeed) > 20f && Math.Abs(currentAngleSpeed) == 0) {
-                    currentAngleSpeed = horizontal * (currentSpeed / speed) * angleSpeed;
-                }
-                Debug.Log("Kar");
-                transform.rotation *= Quaternion.Euler(0f, (currentSpeed / speed) * currentAngleSpeed * Time.deltaTime, 0f);
-                currentAngleSpeed /= 1.001f;
-                if (currentAngleSpeed < 0.4)
-                {
-                    currentAngleSpeed = 0;
-                }
-                //acceleration /= 1.05f;
-            }
-            else {*/
-            transform.rotation *= Quaternion.Euler(0f, (currentSpeed / speed) * horizontal * angleSpeed * Time.deltaTime, 0f);
+        {
             transform.Translate(currentSpeed * Vector3.forward * Time.deltaTime);
-       // }
-       // }
-        /*Vector3 movement = new Vector3(0, 0 , vertical * speed * Time.deltaTime * Math.cos(transform.get));
-        Debug.Log(horizontal);
-        rb.MovePosition(transform.position + movement);
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation *= Quaternion.Euler(0f, -50f * Time.deltaTime, 0f);
+            vecLast = transform.position;
         }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation *= Quaternion.Euler(0f, 50f * Time.deltaTime, 0f);
-        }*/
-        //   while (Math.abs(currentSpeed) > 0)
-        //   {
-        //transform.Translate(currentSpeed * Vector3.forward * Time.deltaTime);
-        //Debug.Log(currentSpeed);
-        //   }
     }
 }
---
+++ Cars And Rings/Assets/Scripts/NewCarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class NewCarController : MonoBehaviour
+{
+
+    private Vector3 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 acceleration = new Vector3(0, 0, 0);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float module = 10f * vertical;
+        float angle = transform.rotation.y;
+        float k = vertical > 0 ? 1 : -1;
+        acceleration = new Vector3(k * (float)Math.Sqrt(module * module / (1 + Math.Sin(angle) * Math.Sin(angle))), 0, k * (float)(Math.Sqrt(module * module / (1 + Math.Sin(angle) * Math.Sin(angle))) * Math.Sin(angle)));
+        speed += acceleration;
+        transform.position += speed * Time.deltaTime;
+        
+    }
+}
